0c4659887bae44702f7ec2f93f533eea2ae70f54
huyvu8051 <huyvu8051@gmail.com>
2022-03-14 17:53:42 +0700

    finish multiple picture in one page

---
+++ src/components/Slide.vue
@@ -0,0 +1,304 @@
+<template>
+  <div id="app">
+    <Tinder
+      ref="tinder"
+      key-name="id"
+      :queue.sync="queue"
+      :offset-y="10"
+      @submit="onSubmit"
+      @mousedown.native="mousedown"
+      @mouseup.native="mouseup"
+      :max="4"
+    >
+      <template slot-scope="scope">
+        <div class="delimiters">
+          <button
+            class="bullet"
+            v-bind:class="{ active: item.isShow }"
+            v-for="(item, index) in scope.data.list"
+            :key="item.id"
+          >
+            {{ index }}
+          </button>
+        </div>
+        <div class="user-info">
+          <span class="fullname"> Rowan Nikolaus </span> <span class="age">20</span> <br> <span class="short-description"> chusng ta cua hien tai, heo kho di nhung ki niem xua kia, ngay mai, nguoi luyen luu theo nhung giac mo tung co, lieu co ta </span>
+        </div>
+
+        <div
+          v-for="item in scope.data.list"
+          :key="item.id"
+          v-show="item.isShow"
+          class="pic"
+          :style="{
+            'background-image': `url(https://cn.bing.com//th?id=OHR.${item.id}_UHD.jpg&pid=hp&w=720&h=1280&rs=1&c=4&r=0)`,
+          }"
+        />
+      </template>
+      <img class="like-pointer" slot="like" src="@/assets/like-txt.png" />
+      <img class="nope-pointer" slot="nope" src="@/assets/nope-txt.png" />
+      <img class="super-pointer" slot="super" src="@/assets/super-txt.png" />
+      <img class="rewind-pointer" slot="rewind" src="@/assets/rewind-txt.png" />
+    </Tinder>
+    <div class="btns">
+      <img src="@/assets/rewind.png" @click="decide('rewind')" />
+      <img src="@/assets/nope.png" @click="decide('nope')" />
+      <img src="@/assets/super-like.png" @click="decide('super')" />
+      <img src="@/assets/like.png" @click="decide('like')" />
+      <img src="@/assets/help.png" @click="decide('help')" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Tinder from "vue-tinder";
+import source from "@/components/bing";
+
+export default {
+  name: "App",
+  components: { Tinder },
+  data: () => ({
+    queue: [],
+    offset: 0,
+    history: [],
+    current: 0,
+    offsetX: 0,
+    offsetY: 0,
+  }),
+  created() {
+    this.mock();
+  },
+  methods: {
+    mousedown(e) {
+      //console.log("mousedown", e);
+
+      this.offsetX = e.offsetX;
+      this.offsetY = e.offsetY;
+    },
+    mouseup(e) {
+      //console.log("mouseup", e);
+
+      if (this.offsetX === e.offsetX && this.offsetY === e.offsetY) {
+        this.queue[0].list[this.current].isShow = false;
+
+        if (e.offsetX > e.srcElement.offsetWidth / 2) {
+          if (this.current < this.queue[0].list.length - 1) {
+            console.log("right");
+            this.current++;
+          }
+        } else {
+          if (this.current > 0) {
+            console.log("left");
+            this.current--;
+          }
+        }
+
+        this.queue[0].list[this.current].isShow = true;
+      }
+    },
+
+    mock(count = 5, append = true) {
+      const list = [];
+      for (let i = 0; i < count; i++) {
+        var rand = Math.floor(Math.random() * 7 + 1);
+        const list2 = [];
+        for (let j = 0; j < rand; j++) {
+          var isShow = j == 0 ? true : false;
+          list2.push({ id: source[this.offset], isShow: isShow });
+          this.offset++;
+        }
+
+        list.push({
+          list: list2,
+          id: Math.random(),
+        });
+      }
+      if (append) {
+        this.queue = this.queue.concat(list);
+      } else {
+        this.queue.unshift(...list);
+      }
+    },
+    onSubmit({ item }) {
+      this.current = 0;
+      if (this.queue.length < 3) {
+        this.mock();
+      }
+      this.history.push(item);
+    },
+    async decide(choice) {
+      if (choice === "rewind") {
+        if (this.history.length) {
+          this.$refs.tinder.rewind([this.history.pop()]);
+        }
+      } else if (choice === "help") {
+        window.open("https://shanlh.github.io/vue-tinder");
+      } else {
+        this.$refs.tinder.decide(choice);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  background-color: #20262e;
+  overflow: hidden;
+}
+
+#app .vue-tinder {
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  right: 0;
+  top: 23px;
+  margin: auto;
+  width: calc(100% - 20px);
+  height: calc(100% - 23px - 65px - 47px - 16px);
+  min-width: 300px;
+  max-width: 355px;
+}
+
+.nope-pointer,
+.like-pointer {
+  position: absolute;
+  z-index: 1;
+  top: 20px;
+  width: 64px;
+  height: 64px;
+}
+
+.nope-pointer {
+  right: 10px;
+}
+
+.like-pointer {
+  left: 10px;
+}
+
+.super-pointer {
+  position: absolute;
+  z-index: 1;
+  bottom: 80px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 112px;
+  height: 78px;
+}
+
+.rewind-pointer {
+  position: absolute;
+  z-index: 1;
+  top: 20px;
+  right: 10px;
+  width: 112px;
+  height: 78px;
+}
+
+.pic {
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+}
+
+.pic2 {
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+}
+
+.btns {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 30px;
+  margin: auto;
+  height: 65px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 300px;
+  max-width: 355px;
+}
+
+.btns img {
+  margin-right: 12px;
+  box-shadow: 0 4px 9px rgba(0, 0, 0, 0.15);
+  border-radius: 50%;
+  cursor: pointer;
+  -webkit-tap-highlight-color: transparent;
+}
+
+.btns img:nth-child(2n + 1) {
+  width: 53px;
+}
+
+.btns img:nth-child(2n) {
+  width: 65px;
+}
+
+.btns img:nth-last-child(1) {
+  margin-right: 0;
+}
+.delimiters {
+  position: absolute;
+
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  display: flex;
+
+  top: 5px;
+}
+.bullet {
+  font-size: 0cm;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.207);
+  border: none;
+  border-radius: 2px;
+
+  vertical-align: middle;
+  margin-left: 2px;
+  margin-right: 2px;
+  display: inline-block;
+  cursor: pointer;
+
+  margin: 2px;
+  height: 4px;
+}
+
+.active {
+  background-color: rgb(255, 255, 255);
+}
+
+.user-info {
+  color: white;
+  position: absolute;
+  bottom: 30px;
+  left: 10px;
+
+  text-align: left;
+}
+.fullname {
+  font-weight: bold;
+  font-size: 32px;
+}
+.short-description{
+  font-size: 17px;
+}
+.age{
+
+  font-size: 23px;
+}
+</style>
